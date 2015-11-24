fdb7de6caa05e988cc6351f5b20098e216e7999e
Joshua Steele <joshukraine@gmail.com>
2015-11-24 10:55:22 +0200

    Clean up generated files.

--- config.rb
+++ config.rb
@@ -1,34 +1,4 @@
 ###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
 # Helpers
 ###
 
@@ -47,11 +17,11 @@
 #   end
 # end
 
-set :css_dir, 'stylesheets'
+set :css_dir, "stylesheets"
 
-set :js_dir, 'javascripts'
+set :js_dir, "javascripts"
 
-set :images_dir, 'images'
+set :images_dir, "images"
 
 # Build-specific configuration
 configure :build do
