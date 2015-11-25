273ae90dea86486331e033deebee4fc056bca6ac
Joshua Steele <joshukraine@gmail.com>
2015-11-25 16:11:18 +0200

    Set up headroom.js

---
+++ source/javascripts/nav.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  var menuToggle = $('#js-mobile-menu').unbind();
+  $('#js-navigation-menu').removeClass("show");
+
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-navigation-menu').slideToggle(function(){
+      if($('#js-navigation-menu').is(':hidden')) {
+        $('#js-navigation-menu').removeAttr('style');
+      }
+    });
+  });
+
+  $("header").headroom({
+    "offset": 50,
+    "tolerance": 5,
+  });
+});
+
