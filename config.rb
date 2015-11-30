0e865bec3a2559216793586736c95520a2e15f1b
Joshua Steele <joshukraine@gmail.com>
2015-11-30 16:36:00 +0200

    Create FAQ page, wire up nav bar for non-index pages.
    * Define full_title helper method for nicer page titles.
    * Create separate layout for non-index pages.
    * Create custom scss and js manifests for index page. This allows choosing certain stylesheets and js scripts that only apply to the index page's custom nav bar.

--- config.rb
+++ config.rb
@@ -9,6 +9,18 @@ set :css_dir, "stylesheets"
 set :js_dir, "javascripts"
 set :images_dir, "images"
 
+helpers do
+  def full_title(page_title=nil)
+    page_title ||= ""
+    base_title = "Carpathian Mountain Outreach"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+end
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
