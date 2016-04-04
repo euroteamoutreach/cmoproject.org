d5432cdd403cc820024b31c65b104ac23aab101b
Joshua Steele <joshukraine@gmail.com>
2016-04-04 17:12:50 +0300

    Update inline_svg method

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -62,29 +62,18 @@ module CustomHelpers
   # https://robots.thoughtbot.com/organized-workflow-for-svg
   # https://gist.github.com/bitmanic/0047ef8d7eaec0bf31bb
   def inline_svg(filename, options = {})
-    asset = sprockets.find_asset(filename)
-    if asset.nil?
-      %(
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 30"
-          width="400px" height="30px"
-        >
-          <text font-size="16" x="8" y="20" fill="#cc0000">
-            Error: '#{filename}' could not be found.
-          </text>
-          <rect
-            x="1" y="1" width="398" height="28" fill="none"
-            stroke-width="1" stroke="#cc0000"
-          />
-        </svg>
-      )
-    else
-      file = asset.source.force_encoding("UTF-8")
+    root = Middleman::Application.root
+    file_path = "#{root}/source/assets/images/#{filename}"
+    if File.exists?(file_path)
+      file = File.read(file_path).force_encoding("UTF-8")
       doc = Nokogiri::HTML::DocumentFragment.parse file
       svg = doc.at_css "svg"
       if options[:class].present?
         svg["class"] = options[:class]
       end
       doc
+    else
+      "file not found: #{file_path}"
     end
   end
 
