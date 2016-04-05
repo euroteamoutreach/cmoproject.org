9e946575a73a87c829e944d55943a77f136f34ca
Joshua Steele <joshukraine@gmail.com>
2016-04-05 10:59:49 +0300

    Convert views to haml

--- config.rb
+++ config.rb
@@ -4,6 +4,7 @@ set :css_dir, "assets/css"
 set :js_dir, "assets/js"
 set :images_dir, "assets/images"
 set :relative_links, true
+set :haml, { ugly: true, format: :html5 }
 
 page "/*.xml", layout: false
 page "/*.json", layout: false
