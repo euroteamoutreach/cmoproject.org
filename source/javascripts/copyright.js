22a262547098b98544b74a1aa6256cccb053e938
Joshua Steele <joshukraine@gmail.com>
2016-04-17 07:47:54 +0300

    Minor footer improvments

--- source/javascripts/copyright.js
+++ source/javascripts/copyright.js
@@ -3,5 +3,5 @@ var moment = require("moment")
 
 $(function() {
   var year = moment().format("YYYY");
-  $("#copyright").html("<p>Copyright &copy; " + year + " <a href=\"http://www.euroteamoutreach.org/\">Euro Team Outreach, Inc.</a></p>");
+  $('.year').append(year)
 });
