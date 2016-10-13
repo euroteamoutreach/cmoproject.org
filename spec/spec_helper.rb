2f71458fdb7bce845f2a464205bf92b8c36966c5
Joshua Steele <joshukraine@gmail.com>
2016-10-13 18:32:04 +0300

    :bug: Correct app env issue in spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,10 +5,10 @@ require "middleman-core/rack"
 require "middleman-s3_sync"
 require "middleman-pry"
 
-middleman_app = ::Middleman::Application.new
-
-Capybara.app = ::Middleman::Rack.new(middleman_app).to_app do
+middleman_app = ::Middleman::Application.new do
   set :root, File.expand_path(File.join(File.dirname(__FILE__), ".."))
-  set :environment, :development
-  set :show_exceptions, false
+  set :environment, :test
+  set :show_exceptions, true
 end
+
+Capybara.app = ::Middleman::Rack.new(middleman_app).to_app
