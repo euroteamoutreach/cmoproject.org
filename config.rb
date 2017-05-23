57f6a543dc957544f9381156c67b87af4dedcc93
Joshua Steele <joshukraine@gmail.com>
2017-05-23 22:08:35 +0300

    Extract prod configs to production env file.

--- config.rb
+++ config.rb
@@ -21,14 +21,6 @@ configure :build do
 
   activate :gzip
 
-  activate :asset_hash
-  activate :asset_host,
-    host: "//d3fljjvmwfqmg4.cloudfront.net",
-    rewrite_ignore: ["*.js"]
-
-  set :url_root, "http://cmoproject.org"
-  activate :search_engine_sitemap
-
   ignore "assets/javascripts/all.js"
   ignore "assets/stylesheets/site"
 
@@ -36,13 +28,4 @@ configure :build do
     html.remove_quotes = false
     html.remove_intertag_spaces = true
   end
-end
-
-activate :s3_sync do |s3|
-  s3.bucket = "cmoproject.org"
-  s3.aws_access_key_id = ENV["AWS_ACCESS_KEY_ID"]
-  s3.aws_secret_access_key = ENV["AWS_SECRET_ACCESS_KEY"]
-  s3.prefer_gzip = true
-  s3.index_document = "index.html"
-  s3.error_document = "404.html"
 end
