24739c7ac84af57bc47c813ead9c156866c69e59
Joshua Steele <joshukraine@gmail.com>
2017-05-25 07:33:18 +0300

    Update AWS env vars.

--- environments/production.rb
+++ environments/production.rb
@@ -9,8 +9,8 @@ activate :search_engine_sitemap
 
 activate :s3_sync do |s3|
   s3.bucket = "cmoproject.org"
-  s3.aws_access_key_id = ENV["AWS_ACCESS_KEY_ID"]
-  s3.aws_secret_access_key = ENV["AWS_SECRET_ACCESS_KEY"]
+  s3.aws_access_key_id = ENV["ETO_AWS_ACCESS_KEY_ID"]
+  s3.aws_secret_access_key = ENV["ETO_AWS_SECRET_ACCESS_KEY"]
   s3.prefer_gzip = true
   s3.index_document = "index.html"
   s3.error_document = "404.html"
