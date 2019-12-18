09c794c2fbd66b377d373cec3abb5bdad08138ae
Joshua Steele <joshukraine@gmail.com>
2019-12-18 07:03:46 +0200

    Add lorem ipsum content for better visibility.

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
 <template>
-  <h1>Home</h1>
+  <div>
+    <h1>Home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe magni officia incidunt error quia, nostrum facere ex aperiam officiis aspernatur? Aspernatur ex illo rem veritatis dolores adipisci deleniti temporibus harum.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe magni officia incidunt error quia, nostrum facere ex aperiam officiis aspernatur? Aspernatur ex illo rem veritatis dolores adipisci deleniti temporibus harum.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe magni officia incidunt error quia, nostrum facere ex aperiam officiis aspernatur? Aspernatur ex illo rem veritatis dolores adipisci deleniti temporibus harum.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe magni officia incidunt error quia, nostrum facere ex aperiam officiis aspernatur? Aspernatur ex illo rem veritatis dolores adipisci deleniti temporibus harum.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe magni officia incidunt error quia, nostrum facere ex aperiam officiis aspernatur? Aspernatur ex illo rem veritatis dolores adipisci deleniti temporibus harum.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe magni officia incidunt error quia, nostrum facere ex aperiam officiis aspernatur? Aspernatur ex illo rem veritatis dolores adipisci deleniti temporibus harum.</p>
+  </div>
 </template>
