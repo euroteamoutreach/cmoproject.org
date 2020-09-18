c80966e2459b8d470c6a3bd79d9e67f8112ffb01
Nathan Day <adayinukraine@gmail.com>
2020-09-18 18:48:29 +0300

    Update Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -101,10 +101,10 @@ gulp.task('images', function() {
 });
 
 // Clean .tmp/
-gulp.task('clean', function() {
+gulp.task('clean', function(done) {
   p.del([
     dest + '*'
-  ]);
+  ]), done();
 });
 
 // Asset Size Report
@@ -118,27 +118,25 @@ gulp.task('sizereport', function () {
 // 4. SUPER TASKS
 
 // Development Task
-gulp.task('development', function(done) {
-  p.runSequence('clean', 'css', 'js', 'images', done);
-});
+gulp.task('development', gulp.series('clean', 'css', 'js', 'images'));
 
 // Production Task
-gulp.task('production', function(done) {
-  p.runSequence('clean', 'css', 'js', 'images', 'sizereport', done);
-});
+gulp.task('production', gulp.series
+  ('clean', 'css', 'js', 'images', 'sizereport')
+);
 
 // Default Task
 // This is the task that will be invoked by Middleman's exteranal pipeline when
 // running 'middleman server'
-gulp.task('default', ['development'], function() {
+gulp.task('default', gulp.series('development', function browsersync () {
 
   p.browserSync.init(serverOpts);
 
-  gulp.watch(css.in, ['css']);
-  gulp.watch(js.in, ['js']);
-  gulp.watch(images.in, ['images']);
+  gulp.watch(css.in, gulp.series('css'));
+  gulp.watch(js.in, gulp.series('js'));
+  gulp.watch(images.in, gulp.series('images'));
 
-});
+}));
 
 function handleError(err) {
   console.log(err.toString());
